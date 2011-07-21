dcbcad599fe8eb6fd93271fc578739b452c12d75
sven.rademakers@gmail.com <sven.rademakers@gmail.com@29be51e6-626f-4f7d-9168-da93946bcd21>
2011-07-21 10:26:15 +0000

    [Sven Rademakers] package edited, now sends encoder values over the network

--- trunk/packages/rosbee_control/src/Tester.cpp
+++ trunk/packages/rosbee_control/src/Tester.cpp
@@ -24,41 +24,93 @@
 #include "rosbee_control/Platform.h"
 #include <string>
 #include <iostream>
+#include <rosbee_control/encoders.h>
+#include <geometry_msgs/Twist.h>
 #include "ros/ros.h"
+#include <sys/time.h>
+
+#define SERIALTIMEOUT 200*1000
+#define PULSEDISTANCEM		15.8/1000	
 
 using namespace std;
 
 Platform* itsPlatform;
 
+
+void move(const geometry_msgs::Twist::ConstPtr& msg)
+{
+	/*
+	int16_t startVal[2];
+	int16_t encVal[2];		
+	int x = 0;
+
+	itsPlatform->read_encoders(startVal);
+	x = msg->linear.x/PULSEDISTANCEM;
+
+	usleep(SERIALTIMEOUT);
+	itsPlatform->move(128,0);
+
+	do
+	{
+		itsPlatform->read_encoders(encVal);
+		encVal[0] -= startVal[0];
+		encVal[1] -= startVal[1];
+
+		usleep(SERIALTIMEOUT);
+
+	}while(encVal[0] < x);
+	
+	itsPlatform->move(0,0); */
+	static timeval last;
+	static float x =0;
+	static float y =0;
+
+	timeval now;
+
+	x += msg->linear.x;
+	y += msg->linear.y;
+
+	gettimeofday(&now,NULL);
+
+	if(last.tv_usec < now.tv_usec - 200000 || last.tv_sec < now.tv_sec)
+	{
+		itsPlatform->move((int)x,(int)(y));
+		gettimeofday(&last,NULL);
+	}	
+}
+
+
 int main(int argc, char** argv)
 {
 	//init ros stuff
 	ros::init(argc,argv,"tester");
-	ros::NodeHandle n;
-	ros::Rate loop_rate(5);
+	ros::NodeHandle n;	
+	ros::Subscriber subx = n.subscribe("cmd_vel",10,move);
+ 	ros::Publisher pub = n.advertise<rosbee_control::encoders>("enc", 1);
+	ros::Rate LoopRate(4);
+	rosbee_control::encoders msg;
+
+	//init platform
 	itsPlatform = Platform::getInstance();
-
-	itsPlatform->connect("/dev/ttyUSB1");
+	itsPlatform->connect("/dev/ttyUSB0");
 	itsPlatform->pc_control(true);
-	usleep(200*1000);
+	usleep(SERIALTIMEOUT);
 	itsPlatform->Enable_motion(true);
-	usleep(200*1000);
-
-	int tellertje = 0;
-	int16_t enc[2];
-
+	usleep(SERIALTIMEOUT);
+	
+	int16_t encVal[2];	
+	
 	while(ros::ok())
 	{
-		if(tellertje%2)itsPlatform->move(tellertje%128,0);
-		else
-		{
-			itsPlatform->read_encoders(enc);
-			ROS_INFO("encoders:%i:::%i",enc[0],enc[1]);
-		}
-		tellertje++;
-		loop_rate.sleep();
+		itsPlatform->read_encoders(encVal);
+		msg.leftEncoder = encVal[0];
+		msg.rightEncoder = encVal[1];
+		pub.publish(msg);
+		
+		ros::spinOnce();
+		LoopRate.sleep();
 	}
-
+	
 }
 
 
