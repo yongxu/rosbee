c08f4385b71aff25f290e02e98d957ed2507560a
sven.rademakers@gmail.com <sven.rademakers@gmail.com@29be51e6-626f-4f7d-9168-da93946bcd21>
2012-01-26 20:26:19 +0000

    -

--- trunk/source/Android/AndroidKeyboard/src/AndroidKeyboard.cpp
+++ trunk/source/Android/AndroidKeyboard/src/AndroidKeyboard.cpp
@@ -13,96 +13,117 @@ using namespace std;
 class UDPServer
 {
 	private:
-		 int sockfd, newsockfd, portnr;
-     socklen_t clilen;
+	int sockfd, newsockfd, portnr;
+    socklen_t clilen;
 
-     struct sockaddr_in serv_addr, cli_addr;
+    struct sockaddr_in serv_addr, cli_addr;
 
-		bool openSocket()
-		{   
-			sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	bool openSocket()
+	{   
+		sockfd = socket(AF_INET, SOCK_STREAM, 0);
 		   if (sockfd < 0) 
 				return false;
 			 return true;
-		}
+	}
 
-	  bool bindServer()
-		{
-			bzero((char *) &serv_addr, sizeof(serv_addr));
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portnr);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+	bool bindServer()
+	{
+		bzero((char *) &serv_addr, sizeof(serv_addr));
+    	serv_addr.sin_family = AF_INET;
+    	serv_addr.sin_addr.s_addr = INADDR_ANY;
+    	serv_addr.sin_port = htons(portnr);
+    	if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) 
              return false;
 		 return true;
 		}
 
-	 	bool AcceptClient()
-	 	{
-				 listen(sockfd,5);
-		   clilen = sizeof(cli_addr);
-		   newsockfd = accept(sockfd, 
-		               (struct sockaddr *) &cli_addr, 
-		               &clilen);
-		   if (newsockfd < 0) 
-					return false;
-				return true;	
-	 	}
+	bool AcceptClient()
+	{
+		listen(sockfd,5);
+		clilen = sizeof(cli_addr);
+		newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr,&clilen);
+		if (newsockfd < 0) 
+			return false;
+		return true;	
+	}
     
    public:
-		UDPServer(int port)
+	UDPServer(int port)
+	{
+		portnr = port;
+		ROS_INFO("Open Sock %i",port);
+		openSocket();
+		ROS_INFO("bind server");
+		bindServer();
+		ROS_INFO("Wainting for CLient on port %i ...",port);
+		if(AcceptClient())
 		{
-			portnr = port;
-			openSocket();
-			bindServer();
-			AcceptClient();			
+			ROS_INFO("Client Connected!");
 		}
-
-		string readLine()
+		else
 		{
-      char buffer[BUFFERLENGHT];
-			bzero(buffer,BUFFERLENGHT);
-    	if(read(newsockfd,buffer,BUFFERLENGHT-1) > 0)
-					return string(buffer);
-			return "";
-		}
-     
+			ROS_INFO("Connection Error!");
+		}		
+	}
 
-		void Close()
-		{			 
-		   close(newsockfd);
-		   close(sockfd);
-		}
+	string readLine()
+	{
+  		char buffer[BUFFERLENGHT];
+		bzero(buffer,BUFFERLENGHT);
+		if(read(newsockfd,buffer,BUFFERLENGHT-1) > 0)
+				return string(buffer);
+		return "";
+	}
+ 
+
+	void Close()
+	{			 
+	   close(newsockfd);
+	   close(sockfd);
+	}
 };
 
-void  StringToValue(double * x, double *y,double *z)
+void  StringToValue(string s,double * x, double *y,double *z)
 {
-
+		ROS_INFO("Incoming:%s",s.c_str());
 }
 
 int main(int argc, char * argv[])
 {
+	ros::init(argc, argv, "AndroidKeyboard");
+	ros::NodeHandle n;
+
+	ROS_INFO("blaat");
   	double walk_vel, run_vel, yaw_rate, yaw_rate_run;
-		UDPServer server = UDPServer();
+	UDPServer server = UDPServer(1234);
 
-		ros::init(argc, argv, "AndroidKeyboard");
-		ros::NodeHandle n;
-
-  	ros::Publisher vel_pub;
-	  geometry_msgs::Twist cmd;
+	ros::Publisher vel_pub;
+	geometry_msgs::Twist cmd;
 		
-		cmd.linear.x = cmd.linear.y = cmd.angular.z = 0;
+	cmd.linear.x = cmd.linear.y = cmd.angular.z = 0;
     vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 
-		n_private.param("walk_vel", walk_vel, 0.5);
-    n_private.param("run_vel", run_vel, 1.0);
-    n_private.param("yaw_rate", yaw_rate, 1.0);
-    n_private.param("yaw_run_rate", yaw_rate_run, 1.5);
+   	//n_private.param("walk_vel", walk_vel, 0.5);
+    //n_private.param("run_vel", run_vel, 1.0);
+    //n_private.param("yaw_rate", yaw_rate, 1.0);
+    //n_private.param("yaw_run_rate", yaw_rate_run, 1.5);
 
-		if(!server.init())
-		return -1;
-		
-		server.close();	
+	ros::Rate loop_rate(50);
+
+	while(ros::ok())
+	{
+		double x,y,z;
+		StringToValue(server.readLine(),&x,&y,&z);
+
+		//publish over ros
+		vel_pub.publish(cmd);
+
+		ros::spinOnce();
+		loop_rate.sleep();
+		cmd.linear.x = cmd.linear.y = cmd.angular.z = 0;
+	}
+
+	server.Close();	
 	return 0;
 }
