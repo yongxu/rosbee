8c663f1bc4aa6304ba45d51058fba8069100a6f3
bram.vdklundert@gmail.com <bram.vdklundert@gmail.com@29be51e6-626f-4f7d-9168-da93946bcd21>
2012-02-09 20:28:54 +0000

    added the StopThread function which causes the thread to shut down (blocking)

--- trunk/source/Android/RosbeeRemote/src/ros/bee/Img_RecvThread.java
+++ trunk/source/Android/RosbeeRemote/src/ros/bee/Img_RecvThread.java
@@ -1,6 +1,7 @@
 package ros.bee;
 
 import java.io.IOException;
+import java.io.InterruptedIOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -10,71 +11,92 @@ import java.net.UnknownHostException;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Handler;
-import android.widget.ImageView;
 
 public class Img_RecvThread extends Thread {
 	private DatagramSocket _socket;
 	private int _port;
 	private InetAddress _ip;
 	private volatile Bitmap bmp;
-	
+	private volatile boolean run;
+
 	private Handler _imgHandler;
-	
+
 	private final int IMGHEIGHT = 120;
 	private final int IMGWIDTH = 160;
 	private final int BUFFSIZE = IMGWIDTH * IMGHEIGHT * 3;
-	
-	public Img_RecvThread(String Ip, int port,Handler imghandler) {
+	private final int SOCKTIMEOUT = 100; // timout for the socket in ms
+
+	public Img_RecvThreadread(String Ip, int port, Handler imghandler) {
 		try {
 			_socket = new DatagramSocket(port);
 			_ip = InetAddress.getByName(Ip);
 			_port = port;
 			_imgHandler = imghandler;
-			
+			_socket.setSoTimeout(SOCKTIMEOUT);
+			run = true;
+
 			System.out.println("IP: " + _ip.toString() + " port: " + _port);
-		}
-		catch (SocketException e) {
+		} catch (SocketException e) {
 			System.out.println("socket exception!!!" + e.toString());
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		bmp = Bitmap.createBitmap(IMGWIDTH,IMGHEIGHT,Bitmap.Config.ARGB_8888); 
-		
+		bmp = Bitmap.createBitmap(IMGWIDTH, IMGHEIGHT, Bitmap.Config.ARGB_8888);
+
 	}
 
 	public void run() {
 		byte[] buffer = new byte[BUFFSIZE];
-		while(true)
-		{
+		boolean readed = true;
+		while (run) {
+			readed = true;
 			try {
 				DatagramPacket packet = new DatagramPacket(buffer, BUFFSIZE);
 				_socket.receive(packet);
-			} 
-			catch (IOException e) {
+
+			} catch (InterruptedIOException e) {
+				if (e.bytesTransferred < IMGHEIGHT * IMGWIDTH * 3)
+					readed = false;
+			} catch (IOException e) {
 				System.out.println(e.toString());
+				readed = false;
 			}
-			
-			int row = 0, col = 0;
-			for(int i = 0; i < BUFFSIZE; i+=3)
-			{
-				bmp.setPixel(col, row,Color.rgb(buffer[i],buffer[i+1],buffer[i+2]));
-				col++;
-				if(col == IMGWIDTH)
-				{
-					row++;
-					col = 0;
+
+			if (readed) {
+				int row = 0, col = 0;
+				for (int i = 0; i < BUFFSIZE; i += 3) {
+					bmp.setPixel(col, row,
+							Color.rgb(buffer[i], buffer[i + 1], buffer[i + 2]));
+					col++;
+					if (col == IMGWIDTH) {
+						row++;
+						col = 0;
+					}
 				}
+				System.out.println("img");
+				_imgHandler.sendEmptyMessage(0);
 			}
-			System.out.println("img");
-			_imgHandler.sendEmptyMessage(0);
 		}
+		run = true;
 	}
-	
-	Bitmap GetImage()
-	{
+
+	Bitmap GetImage() {
 		return bmp;
 	}
-	
+
+	boolean StopThread() {
+		run = false;
+		while(!run) //sleep until the thread ended
+		{
+			try {
+				sleep(10);
+			} catch (InterruptedException e) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
+
